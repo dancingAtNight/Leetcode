f10380e6d54ea25fc355d6e00be03dc805ec81f0
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-06-24 16:23:02 -0700

    Time: 1 ms (100.00%), Space: 44.7 MB (52.48%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -15,25 +15,22 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List< List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
-        allPath(root, targetSum, curr, res);
+        helper(root, targetSum, curr, res);
         return res;
-        
     }
-    private static void allPath(TreeNode root, int target, List<Integer> currPath, List<List<Integer>> allPath){
+    private static void helper(TreeNode root, int sum, List<Integer> currPath, List<List<Integer>> allPath){
+        
         if(root == null) return;
         currPath.add(root.val);
-        
-        if(root.val == target && root.left == null && root.right == null) 
-        {allPath.add(new ArrayList<>(currPath));
+        if(root.val == sum && root.left == null&& root.right == null){
+            allPath.add(new ArrayList<>(currPath));
         }else{
-            allPath(root.left, target- root.val, currPath, allPath);
-            allPath(root.right, target- root.val, currPath, allPath);
+            helper(root.left, sum - root.val, currPath, allPath);
+            helper(root.right, sum - root.val, currPath, allPath);
         }
         
         currPath.remove(currPath.size() -1);
-       
-        
     }
 }
