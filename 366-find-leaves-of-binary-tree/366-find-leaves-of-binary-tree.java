4dc7dc4bbf21b912d5cacdc594f90771601f4ff9
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-06-28 10:33:30 -0700

    Time: 1 ms (60.09%), Space: 43 MB (7.69%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
@@ -14,33 +14,25 @@
  * }
  */
 class Solution {
-   private List<Pair<Integer, Integer>> pairs;
-    private int getHeight(TreeNode root){
-    if(root == null) return -1;
-    int leftHeight = getHeight(root.left);
-    int rightHeight = getHeight(root.right);
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(res,root);
+        return res;
+    }
+    private int helper(List<List<Integer>> res, TreeNode root){
+        if(root == null) return -1;
+        int left = helper(res, root.left);
+        int right = helper(res, root.right);
+        int level = Math.max(left, right) + 1;
+        if(res.size() == level){
+            res.add(new ArrayList<>());
+        }
+        res.get(level).add(root.val);
+        root.left = null;
+        root.right = null;
         
-    int currHeight = Math.max(leftHeight, rightHeight) + 1;
-        
-    this.pairs.add(new Pair<Integer, Integer> (currHeight,root.val));
-        return currHeight;
+    return level;
     
     }
-    public List<List<Integer>> findLeaves(TreeNode root){
-        this.pairs = new ArrayList<>();
-        getHeight(root);
-        Collections.sort(this.pairs, Comparator.comparing(p-> p.getKey()));
-        int n = this.pairs.size(), height= 0, i= 0;
-        List<List<Integer>> solution = new ArrayList<>();
-        while(i < n){
-            List<Integer> nums = new ArrayList<>();
-            while(i < n && this.pairs.get(i).getKey() == height){
-                nums.add(this.pairs.get(i).getValue());
-                i++;
-            }
-            solution.add(nums);
-            height++;
-        }   
-        return solution;
-}
+  
 }
