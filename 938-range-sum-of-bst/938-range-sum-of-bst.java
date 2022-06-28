3f6c755019868fb9cda62cc2bd5780db7d837d36
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-06-28 12:31:21 -0700

    Time: 2 ms (36.98%), Space: 67.1 MB (44.62%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.java
+++ 938-range-sum-of-bst/938-range-sum-of-bst.java
@@ -14,21 +14,17 @@
  * }
  */
 class Solution {
+  
     public int rangeSumBST(TreeNode root, int low, int high) {
-        
-        if(root == null) return 0;
-        int sum = 0;
-        Queue<TreeNode> queue =  new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            TreeNode curr = queue.poll();
-            if(curr.val >= low && curr.val <= high){
-                sum += curr.val;
-            }
-            
-            if(curr.left != null) queue.offer(curr.left);
-            if(curr.right!= null) queue.offer(curr.right);
+       if(root == null) return 0;
+        int left = rangeSumBST(root.left, low, high);
+        int right = rangeSumBST(root.right, low, high);
+        int sum = left+right;
+        if(root.val >= low && root.val <= high){
+            sum +=  root.val;
         }
+        //System.out.println("returning sum=" + sum + " at " + root.val);
         return sum;
+        
     }
 }
