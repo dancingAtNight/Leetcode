313927dc665a66d157a370fda3c79dbb7f36bf9c
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-06-30 12:47:07 -0700

    Time: 82 ms (46.24%), Space: 114.1 MB (18.57%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int left = 0;
+        int maxSum = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int right = 0; right < fruits.length; right++){
+            //put each fruit into the basket
+            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+           
+            while(map.size()> 2){
+               map.put(fruits[left], map.get(fruits[left])-1);
+                if(map.get(fruits[left]) == 0){
+                    map.remove(fruits[left]);
+                
+                }
+                left++;
+            }
+                
+             maxSum = Math.max(maxSum, right - left + 1);
+                
+            }
+            
+        return maxSum;
+        
+    }
+}
