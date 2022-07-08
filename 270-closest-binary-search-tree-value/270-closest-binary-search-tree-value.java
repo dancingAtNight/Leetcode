7f88aa4f199cf880c93cba2c0d21bf60c163b5fd
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-07-08 13:46:04 -0700

    Time: 0 ms (100.00%), Space: 43.7 MB (44.50%) - LeetHub

---
+++ 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int res = 0;
+    public int closestValue(TreeNode root, double target) {
+    res = root.val;
+    traverse(root, target);
+    return res;
+        
+    }
+    void traverse(TreeNode root, double target){
+        if(root == null){
+            return;
+        }
+        if(Math.abs(root.val - target) < Math.abs(res - target)) {
+            res = root.val;
+        }
+        if(root.val < target){
+            traverse(root.right, target);
+        }else{
+            traverse(root.left, target);
+        }
+        }
+    }
