1d64aa7042700125158dcc349ef10276cea47964
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-07-19 10:18:42 -0700

    Time: 1 ms (57.28%), Space: 41.7 MB (77.46%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,16 +1,23 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-       int width = obstacleGrid[0].length;
-    int[] dp = new int[width];
-    dp[0] = 1;
-    for (int[] row : obstacleGrid) {
-        for (int j = 0; j < width; j++) {
-            if (row[j] == 1)
-                dp[j] = 0;
-            else if (j > 0)
-                dp[j] += dp[j - 1];
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        if(grid[0][0] == 1) return 0;
+        grid[0][0] = 1;
+        for(int i = 1; i < m; i++){
+            grid[i][0] = grid[i][0] == 0 && grid[i-1][0] ==1? 1: 0;
         }
+        for(int i = 1; i < n; i++){
+            grid[0][i] = grid[0][i] == 0 && grid[0][i-1] == 1? 1: 0;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(grid[i][j] == 0) {
+                    grid[i][j] = grid[i-1][j] + grid[i][j-1];
+                }else{
+                    grid[i][j] = 0;
+                }
+            }
+        }
+        return grid[m-1][n-1];
     }
-    return dp[width - 1];
-}
 }
