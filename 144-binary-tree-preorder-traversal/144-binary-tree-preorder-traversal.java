415015bc3033e1ed22e0c87f22efda42dc4e85e3
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-07-20 21:02:52 -0700

    Time: 0 ms (100.00%), Space: 42 MB (71.14%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -14,15 +14,14 @@
  * }
  */
 class Solution {
-    List<Integer>  res = new ArrayList<>();
+    ArrayList<Integer> res = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
         helper(root);
         return res;
-       
         
     }
-    public void helper(TreeNode root){
-        if(root == null) return;
+    private void helper(TreeNode root){
+        if(root == null) return ;
         res.add(root.val);
         helper(root.left);
         helper(root.right);
