04b16781c2c6e70d791968f3a225cf77f780379c
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-07-20 21:04:53 -0700

    Time: 0 ms (100.00%), Space: 42.2 MB (48.76%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -14,16 +14,14 @@
  * }
  */
 class Solution {
-    List<Integer> res = new ArrayList<>();
+    ArrayList<Integer> res = new ArrayList<>();
     public List<Integer> postorderTraversal(TreeNode root) {
-        if(root == null) return res;
         helper(root);
         return res;
         
-        
     }
     private void helper(TreeNode root){
-        if(root == null) return;
+        if(root == null) return ;
         helper(root.left);
         helper(root.right);
         res.add(root.val);
