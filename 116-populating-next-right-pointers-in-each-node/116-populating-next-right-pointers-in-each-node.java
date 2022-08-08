be53fa9a35448abf04ff4c4dfc0450adcc2053c5
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-08-08 10:56:33 -0700

    Time: 4 ms (33.92%), Space: 46.6 MB (81.96%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -24,28 +24,15 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         if(root == null) return null;
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int size = queue.size();
-           
-            for(int i = 0; i < size; i++){
-                Node curr = queue.poll();
-                 if(i < size -1){
-                     curr.next = queue.peek();
-                 }
-                if(curr.left != null){
-                    queue.add(curr.left);
-                }
-                if(curr.right != null){
-                queue.add(curr.right);
-                }
-               
-            }
-           
-        }
-         return root;
+        traverse(root.left, root.right);
+        return root;
         
-        
+    }
+    void traverse(Node node1, Node node2){
+        if(node1 == null || node2 == null) return;
+        node1.next = node2;
+        traverse(node1.left, node1.right);
+        traverse(node2.left, node2.right);
+        traverse(node1.right, node2.left);
     }
 }
