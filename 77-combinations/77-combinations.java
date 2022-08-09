b032f1f1b51e361f7f7e15d49b92c7f4f5f1d4b0
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-08-09 13:15:42 -0700

    Time: 29 ms (65.65%), Space: 55.3 MB (30.81%) - LeetHub

---
+++ 77-combinations/77-combinations.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+ 
+    public List<List<Integer>> combine(int n, int k) {
+        if(k <= 0 || n <= 0) return res;
+        List<Integer> path = new ArrayList<>();
+        backtrack(n, k,path, 1);
+        return res;
+        
+    }
+    public void backtrack(int n, int k, List<Integer> path, int start){
+        if(path.size() == k){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = start; i <= n; i++){
+            path.add(i);
+            backtrack(n, k,path, i+1);
+           path.remove(path.size() -1);
+        }
+        
+    }
+}
