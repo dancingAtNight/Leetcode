a27313bff55142fff162559bd2eec5f3aec0c3b2
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-08-11 12:39:13 -0700

    Time: 5 ms (89.21%), Space: 43.3 MB (44.17%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,24 +1,31 @@
 class Solution {
-   public static String multiply(String num1, String num2) {
-        int m = num1.length(), n = num2.length();
-        int[] res = new int[m + n];
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                int sum = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                int p1 = i + j, p2 = i + j + 1;
-                sum += res[p2];
-                res[p2] = sum % 10;
-                res[p1] += sum / 10;
-            }
-        }
-        int i = 0;
-        while (i < res.length && res[i] == 0) {
+    public String multiply(String num1, String num2) {
+        //最长的只能是num1.length + num2.length;
+        if(num1 == "0" || num2 == "0") return "0";
+        int len1 = num1.length();
+        int len2 = num2.length();
+        int[] res= new int[len1 + len2];
+        for(int i = len1 - 1; i >= 0; i--){
+            for(int j = len2 -1; j >= 0; j--){
+                int product = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j + 1, p2 = i + j;
+                product  += res[p1];
+                res[p1] = product % 10;
+                res[p2] += product /10;
+                
+            }    
+    }
+        StringBuilder sb = new StringBuilder();
+      int i = 0;
+        while(i < res.length && res[i] == 0){
             i++;
         }
-        StringBuilder sb = new StringBuilder();
-        for (; i < res.length; i++) {
-            sb.append(res[i] + "");
+        while(i < res.length){
+            sb.append(res[i]);
+            i++;
+
         }
-        return sb.length() == 0 ? "0" : sb.toString();
-    }
+        
+        return sb.length() == 0? "0": sb.toString();
+}
 }
