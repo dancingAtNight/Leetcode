7fde0b0ba4ac2a78f9eec036291874a31b4f9dc4
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-08-15 11:27:37 -0700

    Time: 0 ms (100.00%), Space: 41.6 MB (7.63%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,12 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n <= 1) return n;
-        int[] f= new int[n+1];
-        f[1] = 1;
-        f[2] = 2;
-        for(int i = 3; i <= n; i++){
-            f[i] = f[i-1] + f[i-2];
-        }
-        return f[n];
+        
+        if (n <= 1) return n;
+        int[] dp = new int[3];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            int sum = dp[1] + dp[2];
+            dp[1] = dp[2];
+            dp[2] = sum;
+}
+        return dp[2];
     }
 }
