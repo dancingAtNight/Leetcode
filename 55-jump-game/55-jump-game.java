b34f4ef833985ecef636482c2d67f787198fdac0
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-08-15 12:31:40 -0700

    Time: 3 ms (67.32%), Space: 68.7 MB (17.56%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,13 +1,16 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        //确定状态（
-        int len = nums.length;
-    int farthest = 0;
-    for(int i = 0; i < len-1; i++){
-        farthest = Math.max(farthest, i+ nums[i]);
-        if(farthest <= i){
-                return false;}
-    }
-        return farthest >= len-1;
+ 
+      public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int farthest = 0;
+        for (int i = 0; i < n - 1; i++) {
+            // 不断计算能跳到的最远距离
+            farthest = Math.max(farthest, i + nums[i]);
+            // 可能碰到了 0，卡住跳不动了
+            if (farthest <= i) {
+                return false;
+            }
+        }
+        return farthest >= n - 1;
     }
 }
