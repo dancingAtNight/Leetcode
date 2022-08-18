f38ad4e57d4cd046e1157f2bd6fab4a786a9b689
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-08-18 12:52:58 -0700

    Time: 4 ms (63.95%), Space: 47.4 MB (46.90%) - LeetHub

---
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+   
+    public List<List<Integer>> levelOrder(Node root) {
+         List<List<Integer>> res = new LinkedList<>();
+        if(root == null) return res;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new LinkedList<>();
+            for(int i = 0; i < size; i++){
+                Node curr = queue.poll();
+                list.add(curr.val);
+                for(Node child: curr.children){
+                    queue.offer(child);
+                }
+                
+            }
+            res.add(list);
+        }
+        return res;
+        
+    }
+        
+        
+    }
+
