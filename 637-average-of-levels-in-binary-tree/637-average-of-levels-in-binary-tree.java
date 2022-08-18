33d7ce59048119d76a2740aa19b6692928a23e9a
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-08-18 12:44:57 -0700

    Time: 5 ms (23.88%), Space: 48.7 MB (8.74%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
@@ -15,29 +15,26 @@
  */
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> result = new ArrayList<>();
-    if (root == null)
-      return result;
+        List<Double> res = new LinkedList<>();
+        Queue<TreeNode> queue= new LinkedList<>();
+        if(root == null) return res;
+        queue.offer(root);
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            Double sum = 0.0;
+            for(int i = 0; i < size; i++){
+                TreeNode curr = queue.poll();
+                sum += curr.val;
+                if(curr.left != null) queue.offer(curr.left);
+                if(curr.right != null) queue.offer(curr.right);
+                
 
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.offer(root);
-    while (!queue.isEmpty()) {
-      int levelSize = queue.size();
-      double levelSum = 0;
-      for (int i = 0; i < levelSize; i++) {
-        TreeNode currentNode = queue.poll();
-        // add the node's value to the running sum
-        levelSum += currentNode.val;
-        // insert the children of current node to the queue
-        if (currentNode.left != null)
-          queue.offer(currentNode.left);
-        if (currentNode.right != null)
-          queue.offer(currentNode.right);
-      }
-      // append the current level's average to the result array
-      result.add(levelSum / levelSize);
+            }
+            res.add(sum / size);
+
+        }
+        return res;
+        
     }
-
-    return result;
-}
 }
