7216cd01d35dcf290ef27e21afff499c7b6430c8
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-08-18 14:39:38 -0700

    Time: 1 ms (41.82%), Space: 41.5 MB (66.84%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,15 +1,12 @@
 class Solution {
-    public double average(int[] salary) 
-    {
-        
-    Arrays.sort(salary);
-        Double sum = 0.0; int  count = 0;
-        for(int i = 1; i < salary.length -1; i++){
-            sum += salary[i];
-            count++;
-            
-        }  
-        return sum /count;
-        
+    public double average(int[] salary) {
+        Double sum = 0.0; 
+        int mini = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for(int num: salary){
+            mini = Math.min(mini, num);
+            max = Math.max(max, num);
+            sum += num;
+}
+        return (sum - max - mini)/(salary.length -2);
     }
 }
