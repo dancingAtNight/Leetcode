ed36cf007720e3b2e46477082b18d5f86a43c8b3
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-08-18 12:11:18 -0700

    Time: 1 ms (98.59%), Space: 42.7 MB (87.64%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
@@ -15,30 +15,27 @@
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        return helper(root);
-       
-    }
-    private List<List<Integer>> helper(TreeNode root){
         List<List<Integer>> res = new LinkedList<>();
-        
-        if(root == null) return res;
         Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null) return res;
         queue.offer(root);
+        
+        
         while(!queue.isEmpty()){
             int size = queue.size();
-        List<Integer> curr = new ArrayList<>();
+            List<Integer> list = new LinkedList<>();
             for(int i = 0; i < size; i++){
-                TreeNode currNode = queue.poll();
-                curr.add(currNode.val);
+                TreeNode curr = queue.poll();
+                list.add(curr.val);
+                if(curr.left != null) queue.offer(curr.left);
+                if(curr.right != null) queue.offer(curr.right);
                 
-                if(currNode.left != null) queue.offer(currNode.left);
-                if(currNode.right != null) queue.offer(currNode.right);
-            
             }
-            res.add(0, new ArrayList<>(curr));
+            res.add(0, new LinkedList<>(list) );
             
-        
         }
         return res;
+        
+        
     }
 }
