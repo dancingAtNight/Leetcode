bf7453fb905ec0d73ac75327f4e658f939832596
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-07 06:45:31 -0700

    Time: 0 ms (100.00%), Space: 46.8 MB (81.05%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -3,34 +3,40 @@ class Solution {
         return new int[]{leftBound(nums, target),  rightBound(nums, target)};
         
     }
-    private static int leftBound(int[] nums, int target){
+    
+    static int leftBound(int[] nums, int target){
         int left = 0, right = nums.length -1;
         while(left <= right){
             int mid = left + (right - left)/2;
             if(nums[mid] == target){
-               right = mid -1;
-            }else if(nums[mid] < target){
-                left = mid + 1;
+                right = mid -1;
             }else if(nums[mid] > target){
                 right = mid -1;
+            }else if(nums[mid] < target){
+                left = mid + 1;
             }
         }
-        if(left >= nums.length || nums[left] != target ) return -1;
+        if(left >= nums.length|| nums[left] != target ){
+            return -1;
+        }
         return left;
     }
-    private static int rightBound(int[] nums, int target){
+    
+    static int rightBound(int[] nums, int target){
         int left = 0, right = nums.length -1;
         while(left <= right){
             int mid = left + (right - left)/2;
             if(nums[mid] == target){
-               left = mid + 1;
-            }else if(nums[mid] < target){
-                left = mid + 1;
+                left = mid +1;
             }else if(nums[mid] > target){
                 right = mid -1;
+            }else if(nums[mid] < target){
+                left = mid + 1;
             }
         }
-        if(right < 0 || nums[right] != target) return -1;
+        if(right < 0 || nums[right] != target ){
+            return -1;
+        }
         return right;
     }
 }
