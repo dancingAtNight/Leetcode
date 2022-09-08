fd94562e3eb9e1a72ce55a1ed001c209b0a0c346
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-08 06:51:12 -0700

    Time: 0 ms (100.00%), Space: 42.6 MB (22.85%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -14,25 +14,17 @@
  * }
  */
 class Solution {
-   public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        TreeNode curr = root;
-        TreeNode pre;
-        while (curr != null) {
-            if (curr.left == null) {
-                res.add(curr.val);
-                curr = curr.right; // move to next right node
-            } else { // has a left subtree
-                pre = curr.left;
-                while (pre.right != null) { // find rightmost
-                    pre = pre.right;
-                }
-                pre.right = curr; // put cur after the pre node
-                TreeNode temp = curr; // store cur node
-                curr = curr.left; // move cur to the top of the new tree
-                temp.left = null; // original cur left be null, avoid infinite loops
-            }
-        }
+    public List<Integer> res = new LinkedList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        helper(root);
         return res;
+        
+        
     }
-};
+    public void helper(TreeNode root){
+        if(root == null) return;
+        helper(root.left);
+        res.add(root.val);
+        helper(root.right);
+    }
+}
