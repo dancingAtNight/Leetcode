7065dccd16a3059ea3fa301d54b81a6a630db516
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-12 07:52:56 -0700

    Time: 16 ms (21.95%), Space: 54.5 MB (56.40%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,22 +1,27 @@
 class Solution {
-    public boolean validPalindrome(String s) {
-        int i =0, j = s.length() -1;
-        while(i < j){
+   public static boolean validPalindrome(String s) {
+       if(s == null || s.length() == 0) {
+           throw new IllegalArgumentException();
+       }
+     int i = 0, j = s.length() -1;
+       while(i < j){
+           if(s.charAt(i) != s.charAt(j)){
+               return (checkPalindrome(s, i, j-1) )|| (checkPalindrome(s, i+1, j));
+           }
+           i++;
+           j--;
+}
+       return true;
+       
+       
+    }
+    public static boolean checkPalindrome(String s, int i, int j){
+        while( i < j){
             if(s.charAt(i) != s.charAt(j)){
-                return (checkPalindrome(s, i, j-1)) || (checkPalindrome(s, i+1, j));
+                return false;
             }
             i++;
             j--;
-        }
-        return true;
-        
-        
-    }
-    private boolean checkPalindrome(String s, int i, int j){
-        while(i < j){
-            if(s.charAt(i++) != s.charAt(j--)){
-                return false;
-            }
         }
         return true;
     }
