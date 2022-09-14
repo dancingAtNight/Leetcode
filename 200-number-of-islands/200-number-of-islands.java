f9b135f063da8ce42aa1cf4c73c9aaeee819d992
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-13 20:17:08 -0700

    Time: 11 ms (18.53%), Space: 57.7 MB (38.46%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,40 +1,35 @@
 class Solution {
-
-  
-   public int numIslands(char[][] grid) {
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
         int res = 0;
-        int m = grid.length, n = grid[0].length;
-        // 遍历 grid
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    // 每发现一个岛屿，岛屿数量加一
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == '1'){
                     res++;
-                    // 然后使用 DFS 将岛屿淹了
                     dfs(grid, i, j);
+
                 }
+
             }
+            
         }
         return res;
+        
+        
     }
-
-    // 从 (i, j) 开始，将与之相邻的陆地都变成海水
-    void dfs(char[][] grid, int i, int j) {
+    private static void dfs(char[][] grid, int i, int j){
         int m = grid.length, n = grid[0].length;
-        if (i < 0 || j < 0 || i >= m || j >= n) {
-            // 超出索引边界
+        if( i < 0 || j < 0 || i >= m || j >= n){
             return;
         }
-        if (grid[i][j] == '0') {
-            // 已经是海水了
+        if(grid[i][j] == '0'){
             return;
         }
-        // 将 (i, j) 变成海水
         grid[i][j] = '0';
-        // 淹没上下左右的陆地
-        dfs(grid, i + 1, j);
+        dfs(grid, i+ 1, j);
         dfs(grid, i, j + 1);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j - 1);
+        dfs(grid, i -1, j);
+        dfs(grid, i, j-1);
     }
 }
