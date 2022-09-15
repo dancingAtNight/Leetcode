05043a0a8760a35086633a3512103459f4dcd301
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-15 12:49:13 -0700

    Time: 2 ms (58.50%), Space: 42.5 MB (55.15%) - LeetHub

---
+++ 760-find-anagram-mappings/760-find-anagram-mappings.java
@@ -0,0 +1,19 @@
+class Solution {
+public static int[] anagramMappings(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums2.length; i++){
+            map.put(nums2[i], i);
+        }
+        for(int i = 0; i < nums1.length; i++){
+            if(map.containsKey(nums1[i])){
+                res[i] = map.get(nums1[i]);
+
+
+            }
+        }
+        return res;
+
+    }
+
+}
