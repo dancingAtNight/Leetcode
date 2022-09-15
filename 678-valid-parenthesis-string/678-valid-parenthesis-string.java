822f88a4ada2d199cc6179e2de617f0c1d6473e7
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-15 07:29:47 -0700

    Time: 2 ms (23.88%), Space: 42 MB (50.70%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.java
@@ -1,36 +1,81 @@
 class Solution {
-   public boolean checkValidString(String s) {
-        Stack<Integer> leftStack = new Stack<>();
-        Stack<Integer> starStack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                leftStack.push(i);
-            }
-            else if (s.charAt(i) == ')') {
-                if (!leftStack.isEmpty()) {
-                    leftStack.pop();
-                }
-                else if (!starStack.isEmpty()) {
-                    starStack.pop();
-                }
-                else {
+    public boolean checkValidString(String s) {
+        
+               /*
+                
+               */
+        if(s == null || s.length() == 0) return false;
+        Stack<Integer> left = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                left.push(i);
+            }else if(s.charAt(i) == '*'){
+                star.push(i);
+            }else{
+                if(!left.isEmpty()){
+                    left.pop();
+                }else if(!star.isEmpty()){
+                    star.pop();
+
+                }else{
                     return false;
                 }
             }
-            else {
-                starStack.push(i);
-            }
+
         }
-        while (!leftStack.isEmpty() && !starStack.isEmpty()) {
-            int li = leftStack.pop();
-            int si = starStack.pop();
-            if (si < li) {
+        /*
+        s = "((*()" false
+        left = (0,1), star = (2);
+        
+        s = "((*)" true
+        left = (0), start = (2)
+        
+        s = "(*()" true
+        left = (0), star = (1)
+        
+        s  = "***((("
+        left = (3,4,5) star = (0,1,2);
+            
+            left.size() == star.size()
+            
+            left.size() < star.size()
+            
+            
+            left.size() > star.size()
+            
+            1. left and star are both empty
+                True
+            2. left is empty and star not empty
+                True
+            3. left not empty and star is empty
+                False
+        */
+        
+        while(!left.isEmpty() && !star.isEmpty()){
+            int l = left.pop();
+            int r = star.pop();
+            if(l > r){
                 return false;
             }
-        }
-        if (!leftStack.isEmpty()) {
-            return false;
-        }
-        return true;
-    }   
+}
+       
+      if(left.isEmpty() && star.isEmpty()){
+          return true;
+
+      }else if(!left.isEmpty() && star.isEmpty()){
+          return false;
+
+      }else{
+          return true;
+      }
+}
+        
+        // --- 
+        // if s = "((*)"  left = (0) star = (2)  
+        
+        
+        
+        
     }
+
