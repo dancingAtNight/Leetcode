0893e79ab141ce5886196b5f3764061f2d876bb2
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-15 08:42:42 -0700

    Time: 7 ms (84.60%), Space: 42.1 MB (95.21%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,40 @@
+class Solution {
+    
+    /*
+    s = "badc"  "baba"
+    t = "baba"  "badc"
+    
+map = {b:b, a:a, d:b, c:a}
+map2 = {b:b, a:a, b:d}
+    
+    */
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
+        
+        for(int i = 0; i< s.length(); i++){
+            if(!map.containsKey(s.charAt(i))){
+                 map.put(s.charAt(i), t.charAt(i));
+            }else{
+               if(map.get(s.charAt(i)) != t.charAt(i)){
+                   return false;
+               }
+            }
+            
+
+        }
+        
+        for(int i = 0; i< s.length(); i++){
+            if(!map2.containsKey(t.charAt(i))){
+                map2.put(t.charAt(i), s.charAt(i));
+            }else{
+                if(map2.get(t.charAt(i)) !=s.charAt(i) ){
+                    return false;
+                }
+            }
+
+        }  
+         return true;
+    }
+   
+}
