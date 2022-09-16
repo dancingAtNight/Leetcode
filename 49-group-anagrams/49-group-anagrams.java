940b74f756c3fae03469043694f87f87897982d4
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-16 07:07:39 -0700

    Time: 11 ms (79.62%), Space: 56 MB (51.28%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -3,17 +3,23 @@ class Solution {
         List<List<String>> res = new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         
-        if(strs == null || strs.length == 0) return res;
-        for(int i = 0; i < strs.length; i++){
-            String str = strs[i];
+        /*
+        "eat"  -> ate
+        
+        
+        */
+        
+        if (strs == null || strs.length == 0) return res;
+        for(String str: strs){
             char[] ch = str.toCharArray();
             Arrays.sort(ch);
             String s = String.valueOf(ch);
+            
             if(!map.containsKey(s)){
                 map.put(s, new ArrayList<>());
+                
             }
-            map.get(s).add(strs[i]);
-            
+            map.get(s).add(str);
         }
         return new ArrayList<>(map.values());
         
