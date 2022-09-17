d994ace99b0d5c0eea364e7bccc26b1d904a6cba
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-17 16:49:08 -0700

    Time: 2 ms (24.80%), Space: 41.7 MB (68.08%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.java
@@ -1,91 +1,45 @@
 class Solution {
     public boolean checkValidString(String s) {
-        
-               /*
-               time complexcity --0(n) + O(n) 
-               space : O(m + n) m is the size of left stack(the number of left parenthesis) and n is the size of the star stack(the number of star)
-                
-                
-                Feedback:
-                --- never explain complexity by saying it's not n^2 and not log(n) so it's O(n)
-                -- 
-                
-               */
         if(s == null || s.length() == 0) return false;
         Stack<Integer> left = new Stack<>();
         Stack<Integer> star = new Stack<>();
         for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '('){
+            char ch = s.charAt(i);
+            if(ch == '('){
                 left.push(i);
-            }else if(s.charAt(i) == '*'){
+                
+                
+            }else if(ch == '*'){
                 star.push(i);
+                
+                
             }else{
                 if(!left.isEmpty()){
                     left.pop();
                 }else if(!star.isEmpty()){
                     star.pop();
-
                 }else{
                     return false;
                 }
             }
-
         }
-        /*
-        s = "((*()" false
-        left = (0,1), star = (2);
-        
-        s = "((*)" true
-        left = (0), start = (2)
-        
-        s = "(*()" true
-        left = (0), star = (1)
-        
-        s  = "***((("
-        left = (3,4,5) star = (0,1,2);
-            
-            left.size() == star.size()
-            
-            left.size() < star.size()
-            
-            
-            left.size() > star.size()
-            
-            1. left and star are both empty
-                True
-            2. left is empty and star not empty
-                True
-            3. left not empty and star is empty
-                False
-        */
         
         while(!left.isEmpty() && !star.isEmpty()){
             int l = left.pop();
-            int r = star.pop();
-            if(l > r){
+            int st = star.pop();
+            if(l > st){
                 return false;
+                
             }
+            
+            
+        }    
+        if(!left.isEmpty()){
+            return false;
         }
-        
-        /*
-        s =  "(((***"
-        left = (0,1,2)
-        
-        s = "((*"
-        left = (0,1) star = (2)
-        
-        s  = "()*"
-        
-         
-        
-        */
-       if(!left.isEmpty() && star.isEmpty())
-          return false;
-      return true;
-}
-        
-        // --- 
-        // if s = "((*)"  left = (0) star = (2)       
+        return true;
         
     }
-
+    
+   
+}
