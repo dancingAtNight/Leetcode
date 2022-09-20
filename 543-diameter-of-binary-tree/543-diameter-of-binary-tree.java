cc681c074dedcb0760fbb0db06289d8aed33b428
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-20 13:11:47 -0700

    Time: 1 ms (67.68%), Space: 43.4 MB (38.91%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -18,26 +18,11 @@ class Solution {
 
 public int diameterOfBinaryTree(TreeNode root) {
     // 对每个节点计算直径，求最大直径
-    traverse(root);
+   maxDepth(root);
     return maxDiameter;
 }
 
-// 遍历二叉树
-void traverse(TreeNode root) {
-    if (root == null) {
-        return;
-    }
-    // 对每个节点计算直径
-    int leftMax = maxDepth(root.left);
-    int rightMax = maxDepth(root.right);
-    int myDiameter = leftMax + rightMax;
-    // 更新全局最大直径
-    maxDiameter = Math.max(maxDiameter, myDiameter);
-    
-    traverse(root.left);
-    traverse(root.right);
-}
-
+// 遍历二叉
 // 计算二叉树的最大深度
 int maxDepth(TreeNode root) {
     if (root == null) {
@@ -45,6 +30,9 @@ int maxDepth(TreeNode root) {
     }
     int leftMax = maxDepth(root.left);
     int rightMax = maxDepth(root.right);
+    maxDiameter = Math.max(maxDiameter, leftMax  + rightMax);
+    
+   
     return 1 + Math.max(leftMax, rightMax);
 }
 
