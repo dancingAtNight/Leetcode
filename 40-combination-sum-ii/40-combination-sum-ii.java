dfd22d3ec4c2ac09455260dc41c361c4bd8a8af5
dancingAtNight <108088548+dancingAtNight@users.noreply.github.com>
2022-09-22 08:34:53 -0700

    Time: 6 ms (59.77%), Space: 43.7 MB (69.05%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<Integer> temp = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[candidates.length];
+        backtracking(candidates, 0, target, 0, temp, res, used);
+        return res;
+        
+    }
+    private void backtracking(int[] candidates, int startIdx, int target, int sum, List<Integer> temp, List<List<Integer>> res, boolean[] used){
+        if(sum == target){
+            res.add(new ArrayList<>(temp));
+            return;
+
+        }if(sum > target){
+            return;
+
+        }
+        Arrays.sort(candidates);
+        for(int i = startIdx; i < candidates.length && sum + candidates[i] <= target; i++){
+            if( i > 0 && candidates[i] == candidates[i-1] && used[i-1] == false){
+                    continue;
+            }
+            sum += candidates[i];
+            temp.add(candidates[i]);
+            used[i] = true;
+            backtracking(candidates, i + 1, target, sum, temp, res, used);
+            used[i] = false;
+            sum -= candidates[i];
+            temp.remove(temp.size() -1);
+
+        }
+    }
+}
